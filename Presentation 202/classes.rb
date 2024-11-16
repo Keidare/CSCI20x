ebaaabe900cd58059af5ff6fb0146ce2b384cf00
Angelo <swailem.lumba@obf.ateneo.edu>
2024-11-16 09:19:32 +0800

    Removed unncessary comments

--- Presentation 202/classes.rb
+++ Presentation 202/classes.rb
@@ -1,4 +1,4 @@
-# Define a method that accepts any object that responds to `quack`
+
 def make_it_quack(duck_like)
   if duck_like.respond_to?(:quack)
     duck_like.quack
@@ -7,7 +7,7 @@ def make_it_quack(duck_like)
   end
 end
 
-# Define two classes with a `quack` method
+
 class Duck
   def quack
     puts "Quack! I'm a Duck."
@@ -20,19 +20,18 @@ class Person
   end
 end
 
-# Another class without a `quack` method
+
 class Cat
   def meow
     puts "Meow! I'm a Cat."
   end
 end
 
-# Create instances
+
 duck = Duck.new
 person = Person.new
 cat = Cat.new
 
-# Call the method
-make_it_quack(duck)    # Output: Quack! I'm a Duck.
-make_it_quack(person)  # Output: Quack! I'm a person pretending to be a duck.
-make_it_quack(cat)     # Output: This doesn't quack!
+make_it_quack(duck)    
+make_it_quack(person)  
+make_it_quack(cat)    
